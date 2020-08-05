c743c1f9b00b38d67d25e35cbe2d3074469292d1
Jitender Singh <jitender@cerebry.co>
2020-08-05 23:56:46 +0530

    Commnents done

--- image.py
+++ image.py
@@ -33,16 +33,19 @@ def main():
     #Photo Folder
     photos_list = os.listdir('./Photos')
     number_of_photos = len(photos_list)
-    print("Total "+str(number_of_photos) + "images found !!")
+    print("Total "+str(number_of_photos) + " images found !!")
     count = 0 
     #For progress bar
     bar = progressbar.ProgressBar(maxval=20, \
          widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
     bar.start()
     for filename in photos_list:
+        #For progress bar in one line
         sys.stdout.write('\r')
         if not (filename.endswith('.png') or filename.endswith('.jpg')) or filename == logo_file:
             continue
+
+        #Manipulating the images
         im = Image.open(os.path.join('Photos', filename))
         width, height = im.size
         temp_logo = logoHeight
@@ -59,9 +62,13 @@ def main():
         im.paste(brushIm, (width - brushWidth, height - brushHeight), brushIm)
         im.paste(logoIm, (width - logoWidth - (int)(0.03 * width), height - (int)(0.02 * height) -logoHeight), logoIm)
         im.save(os.path.join('withlogo', filename))
+
+        #Updating progress bar
         count = count + 1
         bar.update(count)
         sys.stdout.flush()
+    
+    #Deleting the tempfiles
     os.remove(logo_file)
     os.remove(brush_file)
     bar.finish()
--- mydecoder.py
+++ mydecoder.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+
+#This will rename the temp file generated to *.png
 def rename():
     renamee = "./Data/temp_logo.nit"
     new_extension = ".png"
@@ -9,6 +11,8 @@ def rename():
     new_extension = ".png"
     pre, ext = os.path.splitext(renamee)
     os.rename(renamee, pre + new_extension)
+
+#This will create simply copy of the file
 def temp_copy():
     shutil.copyfile('./Data/brush.nit', './Data/temp_brush.nit')
     shutil.copyfile('./Data/logo.nit', './Data/temp_logo.nit')

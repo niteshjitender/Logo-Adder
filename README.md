0ed5b979b2058e3227c88e6bc46bfe66bcbbc0df
Jitender Singh <jitender@cerebry.co>
2020-08-05 21:42:21 +0530

    Code is completed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Logo-Adder
+1. Please Add "logo.png" in Logo Folder
+2. Add All the Pictures to 'Photos' Folder
+3. Now run the file
+4. Your all processed pictures will be in the 'withlogo' folder.
--- image.py
+++ image.py
@@ -1,5 +1,29 @@
+import os
+from PIL import Image
+
 def main():
     print("Hello World")
+    sq_fit_size = 300
+    logo_file = 'logo.png'
+    logoIm = Image.open(logo_file)
+    logoWidth, logoHeight = logoIm.size
+    os.makedirs('withlogo', exist_ok = True)
+    for filename in os.listdir('./Photos'):
+        if not (filename.endswith('.png') or filename.endswith('.jpg')) or filename == logo_file:
+            continue
+        im = Image.open(filename)
+        width, height = im.size
+        if width > sq_fit_size and height > sq_fit_size:
+            if width > height:
+                height = int((sq_fit_size / width) * height)
+                width = sq_fit_size
+            else:
+                width = int((sq_fit_size / height) * width)
+                height = sq_fit_size
+            print('Resizing %s'% (filename)) 
+            im = im.resize((width, height))
+            im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+            im.save(os.path.join('withlogo', filename))
 
 if __name__=="__main__":
     main()
